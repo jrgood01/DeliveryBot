9f0ae7605f082b455544751d12f17ecd9f99dc9c
jake <jhaygood7@gatech.edu>
2022-07-01 17:09:03 -0400

    init commit

---
+++ ci.py
@@ -0,0 +1,5 @@
+import os
+
+os.system("git clone https://github.com/jrgood01/DeliveryBot.git")
+os.system("git checkout prod")
+os.system("make")
---
+++ makefile
@@ -0,0 +1,2 @@
+g++ -c main.cpp
+g++ -o robot_service main.o
