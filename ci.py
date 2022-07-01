a24fa94a8c58d81ed20659c2670eaf1741136b04
jake <jhaygood7@gatech.edu>
2022-07-01 17:14:53 -0400

    init

--- ci.py
+++ ci.py
@@ -1,5 +1,5 @@
 import os
 
-os.system("git clone https://github.com/jrgood01/DeliveryBot.git")
-os.system("git checkout prod")
+os.system("git clone https://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@github.com/jrgood01/DeliveryBot.git")
+os.system("git checkout main")
 os.system("make")
