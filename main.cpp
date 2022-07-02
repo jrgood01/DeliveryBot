8fcad679ca0ece737b7d361c71e4bcb8862d903d
jake <jhaygood7@gatech.edu>
2022-07-02 19:17:00 -0400

    Switch project to c++

--- main.cpp
+++ main.cpp
@@ -10,7 +10,7 @@
 void init() {
     sensor_data = (sensor_axis_t*) malloc(sizeof(int) * 3);
     pthread_mutex_init(&sensor_data_mutex, NULL);
-    accelerometer_id = open(ACCELEROMETER_DEVICE_STRINGm O_RDWR);
+    accelerometer_id = open(ACCELEROMETER_DEVICE_STRING, O_RDWR);
 }
 
 int main() {
