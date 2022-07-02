593ed9e7a7e196aead39659b394e48af74b2d71a
jake <jhaygood7@gatech.edu>
2022-07-02 19:18:24 -0400

    Switch project to c++

--- drivers/accelerometer.cpp
+++ drivers/accelerometer.cpp
@@ -4,10 +4,11 @@
 #include <stdlib.h>
 #include "accelerometer.hpp"
 #include "../mutex.hpp"
+#include "../devices.hpp"
 
 //Get the data from the accelerometer
 sensor_axis_t* get_data(int device) {
-    sensor_axis_t* data = malloc(sizeof(int) * 3);
+    sensor_axis_t* data = (sensor_axis_t*) malloc(sizeof(int) * 3);
     ioctl(device, GSENSOR_IOCTL_GETDATA, data);
     return data;
 }
@@ -24,7 +25,7 @@ void update_accelerometer_data(int device) {
 
 //Read accelerometer data from mutex protected memory
 sensor_axis_t* read_accelerometer_data(int device) {
-    sensor_axis_t* data = malloc(sizeof(int) * 3);
+    sensor_axis_t* data = (sensor_axis_t*) malloc(sizeof(int) * 3);
     pthread_mutex_lock(&sensor_data_mutex);
     data->x = sensor_data->x;
     data->y = sensor_data->y;
