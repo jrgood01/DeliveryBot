bc0aa1e54eccb055e04fea2ee97bf1424d584e30
jake <jhaygood7@gatech.edu>
2022-07-01 21:28:58 -0400

    commit

--- test/test_accelerometer.c
+++ test/test_accelerometer.c
@@ -3,13 +3,16 @@
 #include <fcntl.h>
 #include "../drivers/accelerometer.h"
 int main() {
-    int device = open("/dev/accelerometer", O_RDWR);
+    char *gsensor_device = "/dev/accel";
+    int device = open(gsensor_device , O_RDWR);
     printf("Got device %d\n", device);
     while (1) {
         struct sensor_axis* data = get_data(device);
+       /*
         printf("X: %d\n", data->x);
         printf("Y: %d\n", data->y);
         printf("Z: %d\n", data->z);
         printf("\n");
+        */
     }
 }
