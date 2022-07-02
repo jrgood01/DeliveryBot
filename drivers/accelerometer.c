6d29a04dee5e08e34912d0568060758f27b62861
jake <jhaygood7@gatech.edu>
2022-07-01 20:48:24 -0400

    changes

--- drivers/accelerometer.c
+++ drivers/accelerometer.c
@@ -2,8 +2,8 @@
 #include <fcntl.h>
 #include "accelerometer.h"
 struct sensor_axis get_data(int device) {
-    struct sensor_axis data = (int*) malloc(sizeof(int) * 3);
-    ioctl(device, GSENSOR_IOCTL_GETDATA, &data);
+    struct sensor_axis* data = (sensor_axis*) malloc(sizeof(int) * 3);
+    ioctl(device, GSENSOR_IOCTL_GETDATA, data);
     return data;
 }
 
