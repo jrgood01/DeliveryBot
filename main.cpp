6e15d73bd8c65e765b45447516843c0d58805194
jake <jhaygood7@gatech.edu>
2022-07-02 19:15:42 -0400

    Switch project to c++

--- main.cpp
+++ main.cpp
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <pthread.h>
 #include <stdlib.h>
-#include <unistd.h>
+#include <fcntl.h>
+
 #include "drivers/accelerometer.hpp"
 #include "mutex.hpp"
 #include "devices.hpp"
