f562bcbe838f474a13e1d8ec402fc262417904da
jake <jhaygood7@gatech.edu>
2022-07-01 21:06:39 -0400

    changes

--- test/test_accelerometer.c
+++ test/test_accelerometer.c
@@ -6,9 +6,9 @@ int main() {
     int device = open("/dev/accelerometer", O_RDWR);
     while (true) {
         struct sensor_axis* data = get_data(0);
-        printf("X: %d\n", data.x);
-        printf("Y: %d\n", data.y);
-        printf("Z: %d\n", data.z);
+        printf("X: %d\n", data->x);
+        printf("Y: %d\n", data->y);
+        printf("Z: %d\n", data->z);
         printf("\n");
     }
 }
