1f98f14c6f8d7af055cbbd82301dfbaa50b8a3c4
jake <jhaygood7@gatech.edu>
2022-07-02 19:13:07 -0400

    Switch project to c++

--- main.cpp
+++ main.cpp
@@ -1,13 +1,13 @@
 #include <stdio.h>
 #include <pthread.h>
 #include <stdlib.h>
-#include <sys/ioctl.h>
+#include <unistd.h>
 #include "drivers/accelerometer.hpp"
 #include "mutex.hpp"
 #include "devices.hpp"
 
 void init() {
-    sensor_data = malloc(sizeof(int) * 3);
+    sensor_data = (sensor_axis_t*) malloc(sizeof(int) * 3);
     pthread_mutex_init(&sensor_data_mutex, NULL);
     accelerometer_id = open(ACCELEROMETER_DEVICE_STRING);
 }
