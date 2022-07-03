212912ce24367a33e3118fd2326135919be5e5ab
jake <jhaygood7@gatech.edu>
2022-07-02 21:45:23 -0400

    update test make

--- test/test_accelerometer.cpp
+++ test/test_accelerometer.cpp
@@ -1,10 +1,11 @@
 #include <stdio.h>
+#include <unistd.h>
 #include "../drivers/accelerometer.hpp"
 int main() {
     Accelerometer* accelerometer = new Accelerometer();
     accelerometer->begin_update_on_interval(1000);
     while(1) {
-        int* ret_data = accelerometer.read_accelerometer_data();
+        int* ret_data = accelerometer->read_accelerometer_data();
         printf("x: %d y: %d z: %d", ret_data[0], ret_data[1], ret_data[2]);
         usleep(100000)
     }
