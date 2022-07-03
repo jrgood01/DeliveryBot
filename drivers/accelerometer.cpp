749c43487648c3ea7e0acc30624bdba56f88d0b7
jake <jhaygood7@gatech.edu>
2022-07-02 20:30:15 -0400

    Switch project to c++

--- drivers/accelerometer.cpp
+++ drivers/accelerometer.cpp
@@ -5,7 +5,7 @@
 #include <stdexcept>
 #include "accelerometer.hpp"
 
-Accelerometer:Accelerometer() {
+Accelerometer::Accelerometer() {
     pthread_mutex_init(&this->sensor_data_mutex, NULL);
     this->device_id = open(this->ACCELEROMETER_DEVICE_STRING, O_RDWR);
     if(this->device_id < 0) {
@@ -17,7 +17,7 @@ Accelerometer:Accelerometer() {
  * Gets data directly from the accelerometer
  * @return an array containing 3 ints: x, y, z
  */
-accelerometer:get_data() {
+accelerometer::get_data() {
     int* data = (int*) malloc(sizeof(int) * 3);
     ioctl(device, GSENSOR_IOCTL_GETDATA, data);
     return data;
@@ -26,7 +26,7 @@ accelerometer:get_data() {
 /*
  * Writes the current accelerometer data into the buffer
  */
-Accelerometer:update_accelerometer_data() {
+Accelerometer::update_accelerometer_data() {
     int* data = get_data(device);
     pthread_mutex_lock(&this->sensor_data_mutex);
     this->x = data->x;
@@ -38,7 +38,7 @@ Accelerometer:update_accelerometer_data() {
 /*
  * Read Accelerometer data from the buffer
 */
-Accelerometer:read_accelerometer_data() {
+Accelerometer::read_accelerometer_data() {
     int data[3];
     pthread_mutex_lock(&this->sensor_data_mutex);
     data[0] = this->x;
@@ -51,7 +51,7 @@ Accelerometer:read_accelerometer_data() {
 /*
  * helper method to update accelerometer buffer on interval
  */
-Accelerometer:update_on_interval(unsigned int* interval) {
+Accelerometer::update_on_interval(unsigned int* interval) {
     while(1) {
         update_Accelerometer_data();
         usleep(interval);
@@ -62,7 +62,7 @@ Accelerometer:update_on_interval(unsigned int* interval) {
  * Updates the Accelerometer async on a specified interval
  * @param interval the interval in microseconds
  */
-Accelerometer:begin_update_on_interval(unsigned int* interval) {
+Accelerometer::begin_update_on_interval(unsigned int* interval) {
     if (update_thread_id) {
         pthread_cancel(this->update_thread_id)
     }
