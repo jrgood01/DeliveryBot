276a255c31ed832c8a0977d965b368c23944cb6a
jake <jhaygood7@gatech.edu>
2022-07-02 21:44:28 -0400

    update test make

--- test/test_accelerometer.cpp
+++ test/test_accelerometer.cpp
@@ -5,7 +5,7 @@ int main() {
     accelerometer->begin_update_on_interval(1000);
     while(1) {
         int* ret_data = accelerometer.read_accelerometer_data();
-        printf("x: %d y: %d z: %d")
+        printf("x: %d y: %d z: %d", ret_data[0], ret_data[1], ret_data[2]);
         usleep(100000)
     }
 }
