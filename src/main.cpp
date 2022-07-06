e0d246fed10619ca6a0efe1373f5687e1eba58fc
jake <jhaygood7@gatech.edu>
2022-07-06 02:36:47 -0400

    Updates

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,7 @@
 #include <fcntl.h>
 
 #include "global_state.h"
-#include "Accelerometer.h"
+#include "drivers/Accelerometer.h"
 
 int main() {
     Accelerometer* robot_accelerometer = new Accelerometer();
