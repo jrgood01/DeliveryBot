da7668db40858c8e819b5022fe48289bf9713e51
jake <jhaygood7@gatech.edu>
2022-07-05 23:59:03 -0400

    Add cmake

--- main.cpp
+++ main.cpp
@@ -3,12 +3,8 @@
 #include <stdlib.h>
 #include <fcntl.h>
 
-#include "drivers/Accelerometer.hpp"
-#include "global_state.hpp"
+#include "global_state.h"
 
-#include "include/oatpp/web/server/HttpConnectionHandler.hpp"
-#include "include/oatpp/network/Server.hpp"
-#include "include/oatpp/network/tcp/server/ConnectionProvider.hpp"
 
 int main() {
     robot_accelerometer = accelerometer();
