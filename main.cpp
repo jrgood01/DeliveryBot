7d3ee2fb09669746b9226bdffb450cde2f09e38c
jake <jhaygood7@gatech.edu>
2022-07-05 23:40:10 -0400

    add oatpp

--- main.cpp
+++ main.cpp
@@ -6,6 +6,10 @@
 #include "drivers/Accelerometer.hpp"
 #include "global_state.hpp"
 
+#include "include/oatpp/web/server/HttpConnectionHandler.hpp"
+#include "include/oatpp/network/Server.hpp"
+#include "include/oatpp/network/tcp/server/ConnectionProvider.hpp"
+
 int main() {
     robot_accelerometer = accelerometer();
 }
