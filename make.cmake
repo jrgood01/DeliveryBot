61260dbeaf92f55cae765d76da21bf101de1e2d6
jake <jhaygood7@gatech.edu>
2022-07-06 00:02:55 -0400

    cmake update

--- make.cmake
+++
@@ -1,9 +0,0 @@
-cmake_minimum_required(VERSION 3.16)
-project(DeliveryBot LANGUAGES CXX)
-
-add_executable(main 
-    main.cpp,
-    drivers/Accelerometer.cpp,
-    global_state.h,
-    drivers/Accelerometer.h
-)
