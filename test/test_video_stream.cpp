3b7160feeee2263df3a589d7ab954b20b49e744d
jake <jhaygood7@gatech.edu>
2022-07-07 02:32:43 -0400

    Updates

--- test/test_video_stream.cpp
+++ test/test_video_stream.cpp
@@ -3,12 +3,10 @@
 #include "drivers/Camera.h"
 int main() {
     Camera* cam = new Camera();
-    while(1) {
-        cam->begin_update_camera();
-        usleep(10000);
-        Mat frame = cam->get_frame();
-        cv::FileStorage fs("cap.jpg", cv::FileStorage::WRITE);
-        fs << "CamData" << frame;
-        fs.release();
-    }
+    cam->begin_update_camera();
+    usleep(10000);
+    Mat frame = cam->get_frame();
+    cv::FileStorage fs("cap.jpg", cv::FileStorage::WRITE);
+    fs << "CamData" << frame;
+    fs.release();
 }
