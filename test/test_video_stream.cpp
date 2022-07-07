56dad07d5e4ab4487d006aae55cd09cb8402417b
jake <jhaygood7@gatech.edu>
2022-07-07 02:31:08 -0400

    Updates

--- test/test_video_stream.cpp
+++ test/test_video_stream.cpp
@@ -5,6 +5,7 @@ int main() {
     Camera* cam = new Camera();
     while(1) {
         cam->begin_update_camera();
+        usleep(10000)
         Mat frame = cam->get_frame();
         cv::FileStorage fs("cap.jpg", cv::FileStorage::WRITE);
         fs << "CamData" << frame;
